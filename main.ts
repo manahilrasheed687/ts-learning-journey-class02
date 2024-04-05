25d942e6b21902523c79a88489c86dc171f103a3
Manahil rasheed <manahilrasheed507@gmail.com>
2024-04-06 03:48:50 +0500

    push my data types inti github

---
+++ main.ts
@@ -0,0 +1,10 @@
+// LET (Declare variables of diffrent data types)
+// let (Declare one time and initialize multiple time)
+
+let username = "Manahil"; //string
+let age = 14; //number
+let isSmart = true; // boolean
+
+// update the value of the 'username' variable
+username = "Manahil Rasheed";
+console.log(username)
